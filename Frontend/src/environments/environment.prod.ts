82b58e039311c420c5e86d259476a2eb43dc489a
hodamadadi <131853840+hodamadadi@users.noreply.github.com>
2024-10-30 11:29:05 +0430

    Add files via upload
    The environment  file updated!

---
+++ Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// export const environment = {
+//   production: true
+// };
+export const environment = {
+  production: true,
+  backendUrl: 'https://cafe-management-system-1.onrender.com',
+};
---
+++ Frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:7000'
+// };
+export const environment = {
+  production: false,
+  backendUrl: 'https://cafe-management-system-1.onrender.com',
+};
