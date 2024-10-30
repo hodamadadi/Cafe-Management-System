f48a2a43487df0c6bf91b597555c3ba7821ecf1d
hodamadadi <131853840+hodamadadi@users.noreply.github.com>
2024-10-30 14:46:14 +0430

    Update environment.ts

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,10 +1,10 @@
-// export const environment = {
-//   production: false,
-//   apiUrl: 'http://localhost:7000'
-// };
 export const environment = {
   production: false,
-  backendUrl: 'http://localhost:7000',
-  apiUrl: 'http://localhost:7000', // Add this line if it doesn't exist
+  apiUrl: 'http://localhost:7000'
 };
+// export const environment = {
+//   production: false,
+//   backendUrl: 'http://localhost:7000',
+//   apiUrl: 'http://localhost:7000', // Add this line if it doesn't exist
+// };
 
