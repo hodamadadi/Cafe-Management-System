820c8ed8b5ed8c2320d89ec643b4034740e40308
hodamadadi <131853840+hodamadadi@users.noreply.github.com>
2024-10-30 12:40:01 +0430

    Update environment.prod.ts
    Update environment.prod.ts

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -3,5 +3,6 @@
 // };
 export const environment = {
   production: true,
-  backendUrl: 'https://cafe-management-system-1.onrender.com',
+  backendUrl: 'https://cafe-management-system-mauve.vercel.app', // Update as necessary
+  apiUrl: 'https://cafe-management-system-mauve.vercel.app', // Add this line if it doesn't exist
 };
