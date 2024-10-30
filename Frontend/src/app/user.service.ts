70107949ee9a80f9894129fc98ca0c7e68f2410c
aliherawi <aliherawi7@gmail.com>
2024-10-30 20:34:59 +0430

    api url updated

--- Frontend/src/app/user.service.ts
+++ Frontend/src/app/user.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private apiUrl = 'https://your-api-url.com/api';
+  private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  getUsers(): Observable<any[]> { 
+  getUsers(): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}/users`);
   }
 
-  update(data: any): Observable<any> { 
+  update(data: any): Observable<any> {
     return this.http.put<any>(`${this.apiUrl}/users/update`, data);
   }
 }
